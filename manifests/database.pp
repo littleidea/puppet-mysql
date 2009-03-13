d7707106be3a921eb2070266e4b2761684db7d02
Andrew Shafer <andrew@reductivelabs.com>
2009-03-13 00:24:33 -0600

    Add require

--- manifests/database.pp
+++ manifests/database.pp
@@ -1,18 +1,20 @@
 define mysql::database($ensure) {
-                         
+
     case $ensure {
         present: {
             exec { "Mysql: create $name db":
                     command => "/usr/bin/mysql --execute=\"CREATE DATABASE $name\";",
-                    unless => "/usr/bin/mysql --execute=\"SHOW DATABASES;\" | grep '$name'"
+                    unless => "/usr/bin/mysql --execute=\"SHOW DATABASES;\" | grep '$name'",
+                    require => Package['mysql-client']
             }
         }
         absent: {
             exec { "Mysql: drop $name db":
                     command => "/usr/bin/mysql --execute=\"DROP DATABASE $name\";",
-                    onlyif => "/usr/bin/mysql --execute=\"SHOW DATABASES;\" | grep '$name'"
-                                                                                                                                                           }
-                                                                                                                                                       }
+                    onlyif => "/usr/bin/mysql --execute=\"SHOW DATABASES;\" | grep '$name'",
+                    require => Package['mysql-client']
+            }
+        }
         default: {
             fail "Invalid 'ensure' value '$ensure' for mysql::database"
         }
